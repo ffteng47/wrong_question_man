85a476df2a309170c4510fc15819d927420e3ec2
ffteng47 <71693683+ffteng47@users.noreply.github.com>
2026-04-27 00:40:34 +0800

    chore: 清理 merge 带入的临时文件（测试脚本、副本目录等）

--- fix_ollama_install.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('/tmp/ollama_install.sh') as f:
-    data = f.read()
-data = data.replace('BINDIR=/usr/local/bin', 'BINDIR=/home/tff/.local/bin')
-data = data.replace('sudo ', '')
-with open('/tmp/ollama_install.sh', 'w') as f:
-    f.write(data)
-print('Done')
